f9f685301a485de7acff009c829544e7555dfeb7
HavenCTO <jaanon@fedora.email>
2026-01-22 23:39:30 -0500

    bump vlm engine version

--- haven_vlm_connector.py
+++ haven_vlm_connector.py
@@ -34,7 +34,7 @@ try:
         "stashapi:stashapp-tools==0.2.58",
         "aiohttp==3.12.13",
         "pydantic==2.11.7",
-        "vlm-engine==0.7.5",
+        "vlm-engine==0.7.6",
         "pyyaml==6.0.2"
     )
     
